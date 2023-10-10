96e4bfdf3ed477c748634c84add44b1556db0d99
Darlan Bomfim <109982294+darlanbbs@users.noreply.github.com>
2023-10-10 08:58:09 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const route = require("./routes/ProductsRoutes");
 const cors = require("cors");
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 app.use(route);
 
 app.listen(3000);
