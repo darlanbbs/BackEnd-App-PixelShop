b5a057124de671b68944158ef1ccec5d7c6d5434
Darlan Bomfim <darlanbs05@gmail.com>
2023-10-09 21:30:22 -0300

    Feat:Controllers dos produtos com crud + sql feito

---
+++ src/controllers/ProductsControllers.js
@@ -0,0 +1,99 @@
+const pool = require("./../connection/Connection");
+
+const listarProdutos = async (req, res) => {
+  try {
+    const { rows } = await pool.query("select * from produtos");
+
+    return res.json(rows);
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
+
+const detalharProduto = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const { rows, rowCount } = await pool.query(
+      "select * from produtos where id = $1",
+      [id]
+    );
+
+    if (rowCount < 1) {
+      return res.status(404).json({ mensagem: "Produto não encontrado" });
+    }
+
+    return res.json(rows[0]);
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
+
+const cadastrarProduto = async (req, res) => {
+  const { quantidade_estoque, nome, preco, descricao } = req.body;
+
+  try {
+    const { rows } = await pool.query(
+      "insert into produtos (nome, preco, quantidade_estoque,descricao) values ($1, $2, $3, $4, ) returning *",
+      [nome, preco, quantidade_estoque, descricao]
+    );
+
+    return res.status(201).json(rows[0]);
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
+
+const atualizarProduto = async (req, res) => {
+  const { id } = req.params;
+  const { quantidade_estoque, nome, preco, descricao } = req.body;
+
+  try {
+    const { rows, rowCount } = await pool.query(
+      "select * from produtos where id = $1",
+      [id]
+    );
+
+    if (rowCount < 1) {
+      return res.status(404).json({ mensagem: "Produto não encontrado" });
+    }
+
+    await pool.query(
+      "update produtos set nome = $1, preco = $2,  quantidade_estoque= $3, descricao = $5 where id = $6",
+      [nome, preco, quantidade_estoque, descricao, id]
+    );
+
+    return res.status(204).send();
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
+
+const excluirProduto = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const { rows, rowCount } = await pool.query(
+      "select * from produtos where id = $1",
+      [id]
+    );
+
+    if (rowCount < 1) {
+      return res.status(404).json({ mensagem: "Produto não encontrado" });
+    }
+
+    await pool.query("delete from produtos where id = $1", [id]);
+
+    return res.status(204).send();
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
+
+module.exports = {
+  listarProdutos,
+  detalharProduto,
+  cadastrarProduto,
+  atualizarProduto,
+  excluirProduto,
+};
--- src/routes/ProductsRoutes.js
+++ src/routes/ProductsRoutes.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const route = express();
+const {
+  atualizarProduto,
+  cadastrarProduto,
+  detalharProduto,
+  listarProdutos,
+  deletarProduto,
+} = require("./../controllers/ProductsControllers");
 
 route.post("/cadastro", (req, res) => {});
 route.post("/login", (req, res) => {});
