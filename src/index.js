0f3e81fd50d45657ce074e26d953b3284f456578
Darlan Bomfim <109982294+darlanbbs@users.noreply.github.com>
2023-10-11 11:08:02 -0300

    Fix:Bug no push

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ const express = require("express");
 const route = require("./routes/ProductsRoutes");
 const cors = require("cors");
 const app = express();
-<<<<<<< HEAD
-=======
 
->>>>>>> 1b2d5bee36d079b8cad42ed07cc5f37858fb5493
 app.use(cors());
 app.use(express.json());
 
 app.use(route);
 
-<<<<<<< HEAD
-app.listen(5000);
-=======
+
 app.listen('5000');
->>>>>>> 1b2d5bee36d079b8cad42ed07cc5f37858fb5493
+
